710463903c755c19ca45d12a8a7f7ec52183355f
coderoo7 <shubhamyadav.70007@gmail.com>
2020-06-29 23:55:25 +0530

    code refactoring

--- scripts/main.js
+++ scripts/main.js
@@ -7,21 +7,42 @@ const header = document.querySelector("header");
 const container = document.getElementById("container");
 const buttonContainer = document.getElementById("button-container");
 
+//Styling DOM nodes using JS  
+body.style.cssText = "display:flex; flex-direction:column; align-items:center";
+header.style.cssText = "padding:0.8%; font-weight: 800; font-size:36px; color:Chocolate"
+container.setAttribute('style','display:flex; flex-flow:row wrap; width: 520px; height:520px; border: 4px solid black');
+buttonContainer.setAttribute('style','padding: 1% 0')
+Array.from(buttonContainer.children).forEach((item) => item.style.cssText = "padding: 10px; margin:0 14px");
+
+/*******************************
+ * Initializing Event Listener *
+ ******************************/
+container.addEventListener('mouseover',fillColor);
+
+buttonContainer.addEventListener('click',(e) => {
+    if(e.target.id === "reset"){
+        showPopup(); 
+        deleteContainerChild();
+        createGrid();
+    }else{
+        toggleColorButton(e);
+    }
+});
+
+
 function toggleColorButton(e){
-    if(e.target.tagName != 'BUTTON') return;
+    if(e.target.tagName !== 'BUTTON') return;
 
     if(e.target.textContent === "Color"){
         e.target.textContent = "Black";
     }
-    
     else{
         e.target.textContent = "Color";
     }
 }
 
-
 function showPopup(){
-    let input = prompt("Enter the size of square \n(B/w 4 to 64)");
+    let input = prompt("Enter the size of square (B/w 4 to 64)");
     if(isNaN(Number(input))){
         alert("Please enter a number");
         showPopup();
@@ -35,10 +56,11 @@ function getRandomColor(){
 }
 
 function fillColor(e){
-    let color = getRandomColor();
-    let btn = document.getElementById('color');
-    
-    if(btn.textContent=== "Black") {color = 'black'}
+    let color = "black";
+
+    if(e.target.className !== "grid-box") return;
+    if(btn.textContent !== "Black")
+        color = getRandomColor();
     
     e.target.style.cssText += `; background: ${color}`;
 }
@@ -53,37 +75,12 @@ function createGrid(){
     for(let i = 1; i<= sizeOfSquare; i++){
         for(let j = 1; j<= sizeOfSquare; j++){
             const div = document.createElement('div');
-            div.addEventListener('mouseenter',fillColor);
+            div.classList.add('grid-box');
             div.style.cssText = `flex: 1 0 ${100/sizeOfSquare}%`;
             container.appendChild(div);
         }
     }
 }
 
-function init(){
-    console.log("init()")
-    
-    {//Styling DOM nodes using JS
-        
-        body.style.cssText = "display:flex; flex-direction:column; align-items:center";
-        header.style.cssText = "padding:0.8%;font-weight: 800; font-size:36px; color:Chocolate"
-        container.setAttribute('style','display:flex; flex-flow:row wrap; width: 520px; height:520px; border: 4px solid black');
-        buttonContainer.setAttribute('style','padding: 1% 0')
-        Array.from(buttonContainer.children).forEach((item) => item.style.cssText = "padding: 10px;margin:0 14px")
-    }
-
-    buttonContainer.addEventListener('click',(e) => {
-        if(e.target.id === "reset"){
-            showPopup(); 
-            deleteContainerChild();
-            createGrid();
-        }else{
-            toggleColorButton(e);
-        }
-    });
-
-    createGrid();
-}
-
-init();
+window.onload = () => createGrid();
 
